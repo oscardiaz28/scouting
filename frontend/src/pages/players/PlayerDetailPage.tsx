836f02d139670cdfa0e952e040a6ebd94ac0e2ea
oscardiaz28 <diazvargasod@gmail.com>
2025-06-16 01:20:28 -0500

    fix responsive

--- frontend/src/pages/players/PlayerDetailPage.tsx
+++ frontend/src/pages/players/PlayerDetailPage.tsx
@@ -182,7 +182,7 @@ export const PlayerDetailPage = () => {
       </div>
 
       {/* Player Stats */}
-      <div className='grid grid-cols-[25fr_75fr] gap-10 mt-5' id='player-stats'>
+      <div className='grid grid-cols-1 lg:grid-cols-[25fr_75fr] gap-10 mt-5' id='player-stats'>
 
         <PlayerSection player={player} />
 
@@ -192,7 +192,7 @@ export const PlayerDetailPage = () => {
           !stats ? (
             <p>Aun no cuenta con estadisticas el jugador</p>
           ) : (
-            <div className='grid grid-cols-4 gap-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pb-10 lg:pb-10'>
               <StatSection title='Pase' posicion={player.posicion} items={pasesStats} />
               <StatSection title='Defensa' posicion={player.posicion} items={defensaStats} />
               <StatSection title='Regate' posicion={player.posicion} items={regateStats} />
@@ -201,10 +201,8 @@ export const PlayerDetailPage = () => {
           )
         )}
       </div>
-
       {/* Player Videos */}
       <div>
-
       </div>
 
     </div>
